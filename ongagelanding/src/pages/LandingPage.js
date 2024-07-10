7aaae6da4f03bafd46116a180aea0d6afebad50b
CaiusBelcescu <80705095+CaiusBelcescu@users.noreply.github.com>
2024-07-10 14:32:27 +0300

    T out

--- ongagelanding/src/pages/LandingPage.js
+++ ongagelanding/src/pages/LandingPage.js
@@ -487,7 +487,7 @@ const LandingPage = () => {
             </>
           ) : (
             <>
-              <GoogleButton onClick={login}>Sign in with Google 🚀T</GoogleButton>
+              <GoogleButton onClick={login}>Sign in with Google 🚀</GoogleButton>
               <LineContainer>
                 <HorizontalLine />
                 <Text>OR</Text>
