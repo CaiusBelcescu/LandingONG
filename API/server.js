2a04737db1d8234179ed293df6d5f8720d5ed24f
silviafiat <silvia.fiat1@gmail.com>
2024-07-26 18:28:19 +0300

    Increase limit for Express

--- API/server.js
+++ API/server.js
@@ -14,7 +14,8 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
